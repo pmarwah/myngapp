b8d1154db24da7c9d73858e95951804284e717d6
Priyanka <priyanka.marwah@live.com>
2017-05-23 11:42:52 -0400

    Created New File

---
+++ js/init.app.js
@@ -0,0 +1,22 @@
+define(['./config.app.js',
+        './../common/common.factory.js',
+        './../common/common.ctrl.js',
+        './../components/header/header.directive.js',
+        './../components/typeAhead/typeAhead.directive.js'
+    ],
+
+    function(config, commonFactory, commonController, header, typeAhead) {
+        'use strict';
+        var init = {
+            initialize: function(){
+                var app = angular.module('app', ['ngRoute']);
+
+                app.config(config);
+                app.factory('dataFactory', commonFactory);
+                app.controller('appController', commonController);
+                app.directive('typeAhead', typeAhead);
+            }
+        }
+        return init;
+    }
+);
